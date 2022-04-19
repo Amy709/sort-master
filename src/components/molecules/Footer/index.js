217073980f144fa810933aecfde90f29d88a9f10
Amit <ak79438@gmail.com>
2022-04-19 16:33:01 +0530

    final commit

--- src/components/molecules/Footer/index.js
+++ src/components/molecules/Footer/index.js
@@ -2,40 +2,21 @@ import React from 'react';
 import './style.css';
 
 const Footer = (props) => {
-    return ( <
-        footer className = "Footer" >
-        <
-        section >
-        Designed and built with { ' ' } <
-        span className = "Footer__Heart" > & hearts; < /span> by{' '} <
-        a
+    return ( 
+        <footer className = "Footer" >
+        <section >
+        Designed and built with { ' ' } 
+        <span className = "Footer__Heart" > & hearts; </span> by{' '} 
+        <a href="#/" target = "_blank" rel = "noopener noreferrer" className = "Footer__Link" >Amit, Bhuvnesh, Gagan </a> </section>
 
-        target = "_blank"
+        <section className = "Footer__Items" >
+        <a href="#/" target = "_blank"
+        rel = "noopener noreferrer"
+        className = "Footer__Link" >Demo </a> 
+        <a href="#/" target = "_blank"
         rel = "noopener noreferrer"
         className = "Footer__Link" >
-        Amit, Bhuvnesh, Gagan <
-        /a> <
-        /section>
-
-        <
-        section className = "Footer__Items" >
-        <
-        a
-
-        target = "_blank"
-        rel = "noopener noreferrer"
-        className = "Footer__Link" >
-        Demo <
-        /a> <
-        a
-
-        target = "_blank"
-        rel = "noopener noreferrer"
-        className = "Footer__Link" >
-        Code <
-        /a> <
-        /section> <
-        /footer>
+        Code </a> </section> </footer>
     );
 };
 
